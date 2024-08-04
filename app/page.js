888451321a36dc0923b6fa24333c89449e755980
sahlbakshi <sahlaajazbakshi@gmail.com>
2024-08-04 17:29:36 -0400

    Fixed prod

--- app/page.js
+++ app/page.js
@@ -5,10 +5,10 @@ export default function Home() {
     <main className="flex flex-row justify-center items-center min-h-screen gap-20 font-uber">
       <div className="flex flex-col text-left mb-20">
         <h1 className="text-3xl mb-2 font-medium">Bald AI</h1>
-        <h2 className="text-2xl mb-10 font-regular">Find out if you're balding</h2>
-        <Image src={'/apple.svg'} width={220} height={200}></Image>
+        <h2 className="text-2xl mb-10 font-regular">Find out if you&apos;re balding</h2>
+        <Image src={'/apple.svg'} width={220} height={200} alt="app store download"></Image>
       </div>
-      <Image className="rounded-3xl mb-20" src={'/logo.png'} width={200} height={200}></Image>
+      <Image className="rounded-3xl mb-20" src={'/logo.png'} width={200} height={200} alt="app logo"></Image>
     </main>
   )
 }
