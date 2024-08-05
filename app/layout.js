6e3a79eaa83042746b358dc15a045e05524e8b37
sahlbakshi <sahlaajazbakshi@gmail.com>
2024-08-04 22:33:47 -0400

    Added metadata

--- app/layout.js
+++ app/layout.js
@@ -1,29 +1,33 @@
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Head from "next/head";
+import { Inter } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
   title: "Bald AI - Are You Balding?",
   description: "Find out if you're balding with Bald AI",
-};
+  openGraph: {
+    title: "Bald AI - Are You Balding?",
+    description: "Find out if you're balding with Bald AI",
+    url: "https://www.baldaiapp.com/",
+    images: [
+      {
+        url: "/logo.png",
+        alt: "Bald AI Logo",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Bald AI - Are You Balding?",
+    description: "Find out if you're balding with Bald AI",
+    image: "/logo.png",
+  },
+}
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
-        <meta property="og:title" content={metadata.title} />
-        <meta property="og:description" content={metadata.description} />
-        <meta property="og:image" content="/logo.png" />
-        <meta property="og:url" content="https://www.baldaiapp.com/" />
-        <meta name="twitter:card" content="" />
-        <meta name="twitter:title" content={metadata.title} />
-        <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content="/logo.png" />
-      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
