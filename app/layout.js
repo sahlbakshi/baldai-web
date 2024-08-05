f70d2b13fd11504fe43f6bb3c009848196e6f718
sahlbakshi <sahlaajazbakshi@gmail.com>
2024-08-04 22:29:23 -0400

    Meta tags

--- app/layout.js
+++ app/layout.js
@@ -1,17 +1,30 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Bald AI - Find out if you're balding",
-  description: "Bald AI - Find out if you're balding",
+  title: "Bald AI - Are You Balding?",
+  description: "Find out if you're balding with Bald AI",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content="/logo.png" />
+        <meta property="og:url" content="https://www.baldaiapp.com/" />
+        <meta name="twitter:card" content="" />
+        <meta name="twitter:title" content={metadata.title} />
+        <meta name="twitter:description" content={metadata.description} />
+        <meta name="twitter:image" content="/logo.png" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
-  );
+  )
 }
