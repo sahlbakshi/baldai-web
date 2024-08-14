c37b5bf3473d1a1e8c59832022651dfa49d8d97f
sahlbakshi <sahlaajazbakshi@gmail.com>
2024-08-14 15:04:51 -0400

    Added link

--- app/page.js
+++ app/page.js
@@ -6,13 +6,19 @@ export default function Home() {
       <div className="flex flex-col text-center md:text-left mb-10 md:mb-20">
         <h1 className="text-3xl md:text-4xl mb-2 mt-2 font-medium text-white">Bald AI</h1>
         <h2 className="text-xl md:text-2xl mb-4 md:mb-10 font-regular text-gray-100">Find out if you&apos;re balding</h2>
-        <Image 
-          src={'/apple.svg'} 
-          width={200} 
-          height={140} 
-          alt="app store download" 
-          className="mx-auto md:mx-0"
-        />
+        <a 
+          href="https://apps.apple.com/us/app/bald-ai-are-you-balding/id6563141135" 
+          target="_blank" 
+          rel="noopener noreferrer"
+        >
+          <Image 
+            src={'/apple.svg'} 
+            width={200} 
+            height={140} 
+            alt="app store download" 
+            className="mx-auto md:mx-0"
+          />
+        </a>     
       </div>
       <Image 
         className="rounded-3xl mb-10 md:mb-20" 
