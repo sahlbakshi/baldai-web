2e644341ea85dc07fd075599aec53a01536d50e3
Saboor Bakshi <saboor.bakshi@icloud.com>
2024-11-03 07:07:10 -0500

    rehauled all pages + implemented referral dashboard

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import { Separator } from "@/components/ui/separator"
+import Footer from "@/components/Footer"
+import "./globals.css"
+
+export const metadata = {
+  title: "Hairloss AI",
+  description: "Find out if you're losing hair with Hairloss AI",
+}
+
+const MaxWidthContainer = ({ children }: { children: React.ReactNode }) => (
+  <div className="max-w-screen px-6 sm:px-8">{children}</div>
+)
+
+export default function RootLayout({ children }) {
+  return (
+    <html className="light font-sfProText" lang="en">
+      <body className="flex flex-col min-h-screen bg-background text-foreground items-center">
+        <main className="flex grow justify-center my-8 sm:my-16">
+          <MaxWidthContainer>{children}</MaxWidthContainer>
+        </main>
+
+        <Separator />
+
+        <MaxWidthContainer>
+          <Footer />
+        </MaxWidthContainer>
+      </body>
+    </html>
+  )
+}
